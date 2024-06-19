3645267a6c1e772b08b0032bfcf5fb5661f590f3
charlie retzler <charlie.retzler@gmail.com>
2024-06-19 08:51:04 -0400

    adding in retry logic

--- Templates/C#/SolutionTemplate/Project/Program.cs
+++ Templates/C#/SolutionTemplate/Project/Program.cs
@@ -9,7 +9,9 @@ namespace Project
 {
     class Program
     {
-        public const string BaseUrl = "http://localhost:7000";
+        //public const string BaseUrl = "http://localhost:7000";
+        public const string BaseUrl = "https://c3weatherapi.azurewebsites.net";
+        public const int MaxRetries = 10;
 
         public static async Task<IList<HttpResponseMessage>> ProcessOneDayAsync(HttpClient client)
         {
@@ -150,6 +152,94 @@ namespace Project
             return allResults;
         }
 
+        public static async Task<List<HttpResponseMessage>> RetryFailedAsync(HttpClient client, List<HttpResponseMessage> failedTasks, int retries = 0)
+        {
+            var results = new List<HttpResponseMessage>();
+
+            if (!failedTasks.Any()) return results;
+
+            var retriedTasks = new List<Task<HttpResponseMessage>>();
+            var retriedUrls = new List<string>();
+            foreach (var failedTask in failedTasks)
+            {
+                retriedUrls.Add(failedTask.RequestMessage.RequestUri.LocalPath);
+                retriedTasks.Add(client.GetAsync(failedTask.RequestMessage.RequestUri));
+            }
+            Console.WriteLine($"Retrying URLs: {string.Join(", ", retriedUrls)}");
+
+            var taskResults = await Task.WhenAll(retriedTasks);
+
+            var successfulRetriedTasks = taskResults.Where(x => x.IsSuccessStatusCode).ToList();
+            var failedRetriedTasks = taskResults.Where(x => !x.IsSuccessStatusCode).ToList();
+
+            if (!failedRetriedTasks.Any())
+            {
+                return successfulRetriedTasks;
+            }
+
+            if (retries >= MaxRetries)
+            {
+                // We have both success and failures
+                return taskResults.ToList();
+            }
+
+            var delay = TimeSpan.FromSeconds(Math.Pow(retries, 2));
+            Console.WriteLine($"Delaying for {delay.TotalSeconds} seconds before retrying");
+            await Task.Delay(delay);
+
+            return await RetryFailedAsync(client, failedRetriedTasks, retries + 1);
+        }
+
+        public static async Task<List<HttpResponseMessage>> ProcessParallelWithBlocksAndRetriesAsync(HttpClient client, DateTime startDate, int daysToLoad)
+        {
+            var urls = new List<string>();
+            for (var index = 0; index < daysToLoad; index++)
+            {
+                var date = startDate.AddDays(index).ToString("yyyy-MM-dd");
+                urls.Add($"{BaseUrl}/{date}");
+            }
+
+            var blocks = CreateUrlBlocks(urls, 10);
+
+            var allResults = new List<HttpResponseMessage>();
+            foreach (var blockUrls in blocks)
+            {
+                var watch = new Stopwatch();
+                watch.Start();
+
+                var tasks = new List<Task<HttpResponseMessage>>();
+                foreach (var blockUrl in blockUrls)
+                {
+                    tasks.Add(client.GetAsync(blockUrl));
+                }
+
+                var taskResults = await Task.WhenAll(tasks);
+
+                var successfulTasks = taskResults.Where(x => x.IsSuccessStatusCode).ToList();
+                var failedTasks = taskResults.Where(x => !x.IsSuccessStatusCode).ToList();
+                var retriedTasks = await RetryFailedAsync(client, failedTasks);
+
+                // TODO: Should we be logging failed tasks too?
+                allResults.AddRange(successfulTasks);
+                allResults.AddRange(retriedTasks);
+
+                watch.Stop();
+
+                // TODO: Should this include all successful / failures and retries?
+                //var successful = taskResults.Count(x => x.IsSuccessStatusCode);
+                //var initialFailures = failedTasks.Count();
+                //var failures = taskResults.Count(x => !x.IsSuccessStatusCode);
+
+                // Approximate, doesn't account for multiple retires
+                var successful = successfulTasks.Count() + retriedTasks.Count(x => x.IsSuccessStatusCode);
+                var failures = failedTasks.Count() + retriedTasks.Count(x => !x.IsSuccessStatusCode);
+
+                Console.WriteLine($"Took {watch.ElapsedMilliseconds / 1000d} seconds to execute with {taskResults.Count()} results. Average time is {watch.ElapsedMilliseconds / (taskResults.Count() * 1000d)}. Success {successful} Failures {failures}.");
+            }
+
+            return allResults;
+        }
+
 
         public static async Task Main(string[] args)
         {
@@ -165,8 +255,10 @@ namespace Project
             //var results = await ProcessOneWeekParallelInLoopAsync(client);
             //var results = await ProcessTwoWeekParallelInLoopWithoutBlocksAsync(client);
             //var results = await ProcessTwoWeekParallelInLoopWithBlocksAsync(client);
-            var results = await ProcessParallelWithBlocksAsync(client, new DateTime(2020, 01, 01), 21);
+            //var results = await ProcessParallelWithBlocksAsync(client, new DateTime(2020, 01, 01), 21);
             //var results = await ProcessParallelWithBlocksAsync(client, new DateTime(2023, 01, 01), 365);
+            var results = await ProcessParallelWithBlocksAndRetriesAsync(client, new DateTime(2023, 01, 01), 365);
+            
 
 
             watch.Stop();
